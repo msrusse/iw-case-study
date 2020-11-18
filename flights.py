2ec7be357ce5c3426dd4fa90f29ad54c6330511c
Mason Russell <mason.russell@okstate.deu>
2020-11-18 00:22:06 -0600

    Okay, now done for the night

--- flights.py
+++ flights.py
@@ -17,10 +17,13 @@ def determineDistanceGroup(row):
 def determineNextDayArrival(row):
     return int(2400-row['DEPTIME'] < 2400-row['ARRTIME'])
 
-flights_df = pd.read_csv('flights.txt', sep='|', index_col=0)
+all_data_df = pd.read_csv('flights.txt', sep='|')
+airline_df = all_data_df[['AIRLINECODE', 'AIRLINENAME']].drop_duplicates().reset_index(drop=True)
+flights_df = all_data_df.drop(['AIRLINENAME', 'ORIGAIRPORTNAME', 'ORIGINCITYNAME', 'ORIGINSTATE', 'ORIGINSTATENAME', 'DESTAIRPORTNAME', 'DESTCITYNAME', 'DESTSTATE', 'DESTSTATENAME'], axis=1)
+
 
 flights_df['DISTANCEGROUP'] = flights_df.apply(determineDistanceGroup, axis=1)
 flights_df['DEPDELAYGT15'] = flights_df['DEPDELAY'].apply(lambda x: 1 if x > 15 else 0)
 flights_df['NEXTDAYARR'] = flights_df.apply(lambda x: determineNextDayArrival if not x['CANCELLED'] else np.nan, axis=1)
-airline_df = flights_df[['AIRLINECODE', 'AIRLINENAME']]
+airline_df = flights_df[['AIRLINECODE', 'AIRLINENAME']].reset_index()
 airline_df['NOTES'] = flights_df['AIRLINENAME'].apply(lambda x: x[x.find('(')+1:x.find(')')] if '(' in x else None)
